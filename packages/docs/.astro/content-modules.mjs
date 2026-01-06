5203a2d6c36a3cbce46b1342b6d262ab7085def9
Jonathan Gao <gsmlg.com@gmail.com>
2026-01-06 12:17:06 +0800

    fix(docs): fix code block text color on home page install section

--- packages/docs/.astro/content-modules.mjs
+++ packages/docs/.astro/content-modules.mjs
@@ -1,8 +1,8 @@
 
 export default new Map([
+["src/content/docs/getting-started.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/theming.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftheming.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/components/alert.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcomponents%2Falert.mdx&astroContentModuleFlag=true")],
-["src/content/docs/getting-started.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/components/badge.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcomponents%2Fbadge.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/components/button.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcomponents%2Fbutton.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/components/card.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcomponents%2Fcard.mdx&astroContentModuleFlag=true")],
